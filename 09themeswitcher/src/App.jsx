949ac0a7cea2be83dad8f86980c5e1a305392b2c
DivyanshuXOR <sonidivyanshu200@gmail.com>
2026-07-17 19:04:26 +0530

    Create a theme switcher Website

---
+++ 09themeswitcher/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './App.css'
+import { ThemeProvider } from './context/theme'
+import { useEffect } from 'react'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
+
+function App() {
+  const [themeMode, setthemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setthemeMode("light")
+  }
+  const darkTheme = () => {
+    setthemeMode("dark")
+  }
+
+  //actual change in Theme
+  useEffect(() => {
+    const html = document.querySelector("html").classList
+    html.remove("light", "dark")
+    html.add(themeMode)
+  }, [themeMode])
+
+  return (
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}} >
+
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+
+    </ThemeProvider>
+  )
+}
+
+export default App
