b461f8e91c28d53934b1b292d662d44718f7593e
DivyanshuXOR <sonidivyanshu200@gmail.com>
2026-07-22 23:59:59 +0530

    commit

--- 12megaBlog/src/conf/conf.js
+++ 12megaBlog/src/conf/conf.js
@@ -1,6 +1,6 @@
 //Production grade approach
 
-const config = {
+const conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
@@ -8,4 +8,4 @@ const config = {
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 }
 
-export default config
+export default conf
