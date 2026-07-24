71865233003d25d36b925d42216b47d4e46cdc89
DivyanshuXOR <sonidivyanshu200@gmail.com>
2026-07-24 22:30:10 +0530

    Major commits

--- 12megaBlog/src/App.jsx
+++ 12megaBlog/src/App.jsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import { Outlet } from 'react-router'
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <div>A blog app with appwrite</div>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setloading(false))
+
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full-block'>
+        <Header />
+        <main>
+        <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
