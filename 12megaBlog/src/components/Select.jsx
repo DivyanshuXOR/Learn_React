de307c17d26abaf0dff2218348edde597e7cd639
DivyanshuXOR <sonidivyanshu200@gmail.com>
2026-07-27 00:25:40 +0530

    Add PostCard and Select components for displaying posts and options

---
+++ 12megaBlog/src/components/Select.jsx
@@ -0,0 +1,30 @@
+import React, { useId} from 'react'
+
+const Select = React.forwardRef(function Select ({
+    label,
+    options,
+    className = "",
+    ...props 
+} , frwdref){
+    const id = useId()
+    return (
+        <div className='w-full'>
+            {label && <label className='inline-block mb-1 pl-1'
+            htmlFor={id}>{label}</label> }
+            <select 
+            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full
+            ${className}`}
+            ref ={frwdref}
+            {...props}
+            id={id}>
+                {options?.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+    )
+})
+
+export default Select
